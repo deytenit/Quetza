ae41f11bef2f5c006f66dcbc066577863ff4edc4
Vladimir (unknowableshade) Eremin <60275123+unknowableshade@users.noreply.github.com>
2023-05-09 03:22:57 +0300

    Docker & Versions

--- src/modules/server/module.ts
+++ src/modules/server/module.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 const name = "server";
 const description = "Server utilities module";
 const author = "unknowableshade";
-const tag = "1.0.0";
+const tag = "0.0.1";
 
 const rootDir = path.dirname(fileURLToPath(import.meta.url));
 
